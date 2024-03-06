028e6d3a01a2718220c0730cc6a93b1576e8ec61
HenriqueZapellaRochagit config --global user.email henri_zap@hotmail.com <henri_zap@hotmail.com>
2024-03-06 20:02:28 -0300

    utilitarioAdicionado

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ public class App {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
-        
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
 
 
         System.out.println("Tipos de veiculos:");
@@ -26,6 +26,14 @@ public class App {
         System.out.println("\nViajando com o carro esportivo");
         esportivo.viaja(250);
         System.out.println(esportivo);
+
+        System.out.println("\n\n----------------");
+        System.out.println("\nAbastencendo carro utilitario com diesel");
+        utilitario.abastece(TipoCombustivel.DIESEL, 70);
+        System.out.println(utilitario);
+        System.out.println("\nViajando com o carro utilitario");
+        utilitario.viaja(250);
+        System.out.println(utilitario);
 
 
     }
